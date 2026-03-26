080dbd653b0971dbcd2529a9dcad740a07ad531b
Nicolas Rotela <nicolas_rotela@hotmail.com>
2026-03-26 12:17:13 -0300

    correciones en version mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import "../styles/navbar.css";
 
-// Barra de navegación del portfolio
-
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="container navbar-container">
-        {/* Logo / Nombre */}
         <a href="#hero" className="logo">
           <img src="/logo.png" alt="NR logo" className="logo-img" />
         </a>
 
+        {/* BOTÓN HAMBURGUESA */}
+        <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+
         {/* Links */}
-        <div className="nav-links">
+        <div className={`nav-links ${menuOpen ? "active" : ""}`}>
           <a href="#sobre-mi">Sobre mí</a>
           <a href="#proyectos">Proyectos</a>
           <a href="#tecnologias">Tecnologías</a>
